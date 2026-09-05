c019ce9779ba8706e841e426084d408be086221f
unknown <10135718@qq.com>
2026-09-05 21:21:36 +0800

    复合赋值运算

---
+++ increase plus 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    int a;
+    a = ++i;
+    printf("%d\n", a);
+    printf("%d\n", i);
+    return 0;
+}
---
+++ increase plus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i = 1;
+
+    int a;
+
+    a = i++;
+    printf("%d\n", a);
+    printf("%d\n", i);
+    return 0;
+}
